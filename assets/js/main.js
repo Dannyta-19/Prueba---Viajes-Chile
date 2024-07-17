e91aeff058f466c2b19fac03230d9884f38da24c
Danny <daniela.berrios.p@gmail.com>
2024-07-16 23:02:48 -0400

    detaller finales y desarrollo de readme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
   /*click en el boton de contacto */
 $("#boton").click(function(){
     alert("El mensaje fue enviado correctamente")
+    
   });
-
-  /* al pasar en el input de contacto */  
-  
+  /* tooltip de contacto*/
 
   $(document).ready(function(){
     $('.escri').tooltip({title: "!!!Cuentanos¡¡¡", animation: false, placement: "bottom"}); 
     $('.efenom').tooltip({title: "Acá debes escribir tu nombre completo", animation: true, placement: "bottom"}); 
     $('.efect').tooltip({title: "Acá debes escribir el asunto de tu mensaje", animation: false, placement: "bottom"}); 
   });
+
+
+ 
